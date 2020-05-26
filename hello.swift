254c46fda163f2f54dd0818c05cb4c5395be732e
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2020-05-26 18:22:51 +0000

    Hello GraphQL swapi

--- hello.swift
+++ hello.swift
@@ -1,1 +1,24 @@
-print("Hello World")
+import Foundation
+import FoundationNetworking
+
+let session = URLSession.shared
+let url = URL(string: "https://swapi.graph.cool/graphql")!
+var request = URLRequest(url: url)
+request.httpMethod = "POST"
+request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+let json = [
+  "query": "{ allFilms { title } }"
+]
+let jsonData = try! JSONSerialization.data(withJSONObject: json, options: [])
+let sem = DispatchSemaphore.init(value: 0)
+
+let task = session.uploadTask(with: request, from: jsonData) { data, response, error in
+  defer { sem.signal() }
+
+  if let data = data, let dataString = String(data: data, encoding: .utf8) {
+    print(dataString)
+  }
+}
+
+task.resume()
+sem.wait()
